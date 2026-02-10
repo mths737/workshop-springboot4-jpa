ded412f36a9a4432fd45c3b85663cf817f96fc37
Matheus Alves <mths737@gmail.com>
2026-02-10 07:35:31 -0300

    Project created

---
+++ src/main/java/com/example/courrse/CourrseApplication.java
@@ -0,0 +1,13 @@
+package com.example.courrse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourrseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourrseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/courrse/CourrseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courrse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourrseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
