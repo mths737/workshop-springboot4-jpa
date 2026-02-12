34f473a2c15f8cadd95bbe7bdde1b48f29f2b085
Matheus Alves <mths737@gmail.com>
2026-02-11 22:57:30 -0300

    OrderStatus enum

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Order;
 import com.example.course.entities.User;
+import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:51Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-19T12:32:12Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-23T06:59:10Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:51Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-19T12:32:12Z"), OrderStatus.SHIPPED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-23T06:59:10Z"), OrderStatus.CANCELED, u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.example.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -27,16 +28,20 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 	
 	public Order() {
 	}
-
-	public Order(Long id, Instant moment, User client) {
+	
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -63,6 +68,16 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();			
+		}
+	}
 
 	@Override
 	public int hashCode() {
@@ -80,6 +95,7 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 
